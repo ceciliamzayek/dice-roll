8a412764e96e6173d0b8dc32d07287a618de19fc
Cecilia <cecilia.mzayek@gmail.com>
2023-06-04 17:41:42 +0400

    Add dice array to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
-import One from '../components/dice/one';
-import Two from '../components/dice/two';
-import Three from '../components/dice/three';
-import Four from '../components/dice/four';
-import Five from '../components/dice/five';
-import Six from '../components/dice/six';
+'use client';
+import DiceInput from '@/components/diceInput';
+import createDice from '../components/dice/diceArray';
+import { useState } from 'react';
 
 export default function Home() {
+  const [diceCount, setDiceCount] = useState(0);
+
+  let dice = createDice(diceCount);
+  console.log(dice);
+
   return (
     <div>
       <h1>Dice Game</h1>
-      <div className="board">
-        <One />
-        <Two />
-        <Three />
-        <Four />
-        <Five />
-        <Six />
-      </div>
+      <DiceInput diceCount={diceCount} setDiceCount={setDiceCount} />
+      <div className="board">{dice}</div>
     </div>
   );
 }
